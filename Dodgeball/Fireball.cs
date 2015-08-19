d01793382755e380268b2543109fd91c733983ee
allen12 <computer_allen@yahoo.com>
2015-08-19 14:33:02 -0400

    Some changes to the Fireball class

--- Dodgeball/Fireball.cs
+++ Dodgeball/Fireball.cs
@@ -14,14 +14,63 @@ namespace Dodgeball
     {
         /* Represents the number of ticks that must pass before a new fireball is generated. 
             "Normal" difficulty is 3. */
-        private int delay;
+        private static int delay;
+
+        private static int lowX = 0;
+        private static int highX = 450;
+        private static int lowY = 40;
+        private static int highY = 40;
+
+        /* A running list of currently active fireballs */
+        private static List<Fireball> fireballs = new List<Fireball>();
 
         private int X;
         private int Y;
 
-        public Fireball(int delay)
+        public Fireball(int startX, int startY)
         {
-            this.delay = delay;
+            X = startX;
+            Y = startY;
+        }
+
+        public static void setFireballDelay(int fireballDelay)
+        {
+            delay = fireballDelay;
+        }
+
+        public static void setBounds(int lX, int hX, int lY, int hY)
+        {
+            lowX = lX;
+            highX = hX;
+            lowY = lY;
+            highY = hY;
+        }
+
+        /// <summary>
+        /// Generate a new fireball with a random starting location and adds it to the running list. 
+        /// Bound arguments are inclusive.
+        /// </summary>
+        /// <param name="lowX">Low x bound</param>
+        /// <param name="highX">High x bound</param>
+        /// <param name="lowY">Low y bound</param>
+        /// <param name="highY">High y bound</param>
+        /// <returns>A new fireball</returns>
+        public static Fireball generateFireball(int lowX, int highX, int lowY, int highY)
+        {
+            if (lowX < 0 || lowY < 0)
+                throw new ArgumentOutOfRangeException("Low bound is out of bounds!");
+
+            Random random = new Random();
+
+            Fireball fireball = new Fireball(random.Next(lowX, highX), random.Next(lowY, highY));
+            fireballs.Add(fireball);
+            return fireball;
+        }
+
+        public static void advanceGameTick(int currentGameTick)
+        {
+            if (currentGameTick % 3 == 0)
+                
         }
     }
 }
