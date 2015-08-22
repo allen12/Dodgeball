4d82bc831c55ff6026b6a01c3182f7ac93ee0660
allen12 <computer_allen@yahoo.com>
2015-08-22 10:46:48 -0400

    Throws exception in different game tick method

--- Dodgeball/Fireball.cs
+++ Dodgeball/Fireball.cs
@@ -16,6 +16,10 @@ namespace Dodgeball
             "Normal" difficulty is 3. */
         private static int delay;
 
+        /* The Y-coordinate limit at which the fireball is removed */
+        private static int remove = 460;
+
+        private static int speed = 2;
         private static int lowX = 0;
         private static int highX = 450;
         private static int lowY = 40;
@@ -33,13 +37,36 @@ namespace Dodgeball
             Y = startY;
         }
 
+        public void advance()
+        {
+            this.Y += speed;
+
+            if (Y >= remove)
+                fireballs.Remove(this);
+        }
+
         public static void setFireballDelay(int fireballDelay)
         {
             delay = fireballDelay;
         }
 
+        public static void setSpeed(int s)
+        {
+            speed = s;
+        }
+
+        /// <summary>
+        /// Set the boundaries of where fireballs are allowed to be generated
+        /// </summary>
+        /// <param name="lX"></param>
+        /// <param name="hX"></param>
+        /// <param name="lY"></param>
+        /// <param name="hY"></param>
         public static void setBounds(int lX, int hX, int lY, int hY)
         {
+            if (lowX < 0 || lowY < 0)
+                throw new ArgumentOutOfRangeException("Low bound is out of bounds!");
+
             lowX = lX;
             highX = hX;
             lowY = lY;
@@ -55,11 +82,8 @@ namespace Dodgeball
         /// <param name="lowY">Low y bound</param>
         /// <param name="highY">High y bound</param>
         /// <returns>A new fireball</returns>
-        public static Fireball generateFireball(int lowX, int highX, int lowY, int highY)
+        public static Fireball generateFireball()
         {
-            if (lowX < 0 || lowY < 0)
-                throw new ArgumentOutOfRangeException("Low bound is out of bounds!");
-
             Random random = new Random();
 
             Fireball fireball = new Fireball(random.Next(lowX, highX), random.Next(lowY, highY));
@@ -69,8 +93,15 @@ namespace Dodgeball
 
         public static void advanceGameTick(int currentGameTick)
         {
-            if (currentGameTick % 3 == 0)
-                
+            // Each tick advances the Y-component of the fireball by some number of pixels
+            foreach (Fireball fb in fireballs)
+            {
+                fb.advance();
+            }
+
+            // Generate a new fireball if current game tick reaches the delay threshold
+            if (currentGameTick % delay == 0)
+                generateFireball();
         }
     }
 }
